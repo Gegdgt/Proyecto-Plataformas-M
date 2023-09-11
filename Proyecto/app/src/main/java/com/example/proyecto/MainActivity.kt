7230364e6d409b7c6ed5175c2aeac033d609fd0f
Gohan1233 <gar21352@uvg.edu.gt>
2023-09-11 15:47:32 -0600

    TopBar Done

--- Proyecto/app/src/main/java/com/example/proyecto/MainActivity.kt
+++ Proyecto/app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.proyecto.main_feed_screen.MainFeedScreen
 import com.example.proyecto.ui.theme.ProyectoTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +18,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ProyectoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                //No esta llamando a la funcion bien y no se porque
+                MainFeedScreen()
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProyectoTheme {
-        Greeting("Android")
-    }
+fun MainActivityPreview() {
+    MainActivity()
 }
---
+++ Proyecto/app/src/main/java/com/example/proyecto/main_feed_screen/MainFeedScreen.kt
@@ -0,0 +1,38 @@
+package com.example.proyecto.main_feed_screen
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainFeedScreen() {
+    Scaffold (
+        topBar = {
+            TopBar()
+        },
+        bottomBar = {
+
+        },
+        //backgroundColor = Color.White,
+
+    ) {innerPadding ->
+        LazyColumn(
+            modifier = Modifier.padding(innerPadding)
+        ){
+
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun MainFeedScreenPreview() {
+    MainFeedScreen()
+}
