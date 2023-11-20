e4ed4bc89e38ec5059b4edd3f3fc6dc15729618f
Javilejo <pra21486@uvg.edu.gt>
2023-11-20 01:35:25 -0600

    guardar display_name en firebase

--- Proyecto/app/src/main/java/com/example/proyecto/Authentication/SignInScreenViewModel.kt
+++ Proyecto/app/src/main/java/com/example/proyecto/Authentication/SignInScreenViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 
@@ -40,6 +41,9 @@ class SignInScreenViewModel: ViewModel() {
             auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful){
+                        val displayName =
+                            task.result.user?.email?.split("@")?.get(0)
+                        createUser(displayName)
                         MainFeedScreen()
                     }
                     else {
@@ -49,6 +53,22 @@ class SignInScreenViewModel: ViewModel() {
                 }
         }
 
+    }
+
+    private fun createUser(displayName: String?) {
+        val userId = auth.currentUser?.uid
+        val user = mutableMapOf<String, Any>()
+
+        user["user_id"] = userId.toString()
+        user["display_name"] = displayName.toString()
+
+        FirebaseFirestore.getInstance().collection("users")
+            .add(user)
+            .addOnSuccessListener {
+                Log.d("BREAK","Creado ${it.id}")
+            }.addOnFailureListener {
+                Log.d("BREAK","Ocurrio un error ${it}")
+            }
     }
 
 }
