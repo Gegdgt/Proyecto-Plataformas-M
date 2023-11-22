732fdb764facfd50c8abce7c45975cbe6318ba6b
Gohan1233 <gar21352@uvg.edu.gt>
2023-11-22 07:53:34 -0600

    Editar imagen perfil

--- Proyecto/app/src/main/java/com/example/proyecto/profile_perfil/ProfileScreen.kt
+++ Proyecto/app/src/main/java/com/example/proyecto/profile_perfil/ProfileScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -103,5 +104,5 @@ fun ProfileScreen(navController: NavController) {
 @Preview
 @Composable
 fun ProfileScreenPreview() {
-    //ProfileScreen()
+    ProfileScreen(rememberNavController())
 }
