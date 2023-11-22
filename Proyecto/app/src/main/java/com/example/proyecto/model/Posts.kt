e386c2afdc699cf9e254f75116142652a2d3f9ff
Javilejo <pra21486@uvg.edu.gt>
2023-11-22 14:33:47 -0600

    subir imagenes a capetas por usarios

--- Proyecto/app/src/main/java/com/example/proyecto/model/Posts.kt
+++ Proyecto/app/src/main/java/com/example/proyecto/model/Posts.kt
@@ -1,3 +1,14 @@
 package com.example.proyecto.model
 
-data class Posts()
+data class Posts(
+    val postId: String,
+    val imageUrl: String,
+
+) {
+    fun toMap(): Map<String, Any>{
+        return mapOf(
+            "post_id" to postId,
+            "image_url" to imageUrl
+        )
+    }
+}
